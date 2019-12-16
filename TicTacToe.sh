ba8b9c0debeda6df5a248b9950cb27edd4d36d5e
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-16 11:03:25 +0530

    ADDED: Toss Function For Player And Computer

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -6,12 +6,19 @@ declare -a gameBoard
 
 gameBoard=(_ _ _ _ _ _ _ _ _)
 
-toss=$((RANDOM%2))
+function toss(){
 
-case $toss in
+	toss=$((RANDOM%2))
 
-	0)
-		player=o;;
-	1)
-		player=x;;
-esac
+	case $toss in
+
+		0)
+			player=o
+			computer=x;;
+		1)
+			player=x
+			computer=o;;
+	esac
+
+	echo $player $computer
+}
