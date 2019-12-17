9748617d0815a6e9b154640695d7c77e7e59746b
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-17 17:00:10 +0530

    ADDED: Function for Winner Condition and Change Turn

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -4,6 +4,8 @@ echo "Welcome to TicTacToe Game"
 
 declare -a gameBoard
 
+changeTurn=x
+
 gameBoard=(_ _ _ _ _ _ _ _ _)
 
 function toss(){
@@ -13,10 +15,24 @@ function toss(){
 	case $toss in
 
 		0)
-			echo o;;
+			player1=o
+			player2=x;;
 		1)
-			echo x;;
+			player1=x
+			player2=o;;
 	esac
+
+	echo $player1 $player2
+}
+
+function changeTurn(){
+
+	if [[ $1 == x ]]
+	then
+		changeTurn=o
+	else
+		changeTurn=x
+	fi
 }
 
 function printBoard(){
@@ -35,20 +51,94 @@ function playerMove(){
 	flag=true
 	while [[ $flag == true ]]
 	do
-		read -p "Enter the Position " pos
+		read -p "Enter the Position $1 " pos
 
-		if (( $pos <= 9 ))
+		if (( $pos <= 9 && ${gameBoard[$pos-1]}!=x &&  ${gameBoard[$pos-1]}!=o ))
 		then
 			gameBoard[$(($pos - 1))]=$1
 			flag=false
 		fi
 	done
+		changeTurn $1
+}
+
+function checkWhoWon(){
+
+	flag=false
+	row=0
+	col=0
+	diag1=0
+	diag2=2
+
+	for (( i=0;i<3;i++ ))
+	do
+		if [[ ${gameBoard[$row]} == $1 && ${gameBoard[$((row=$row+1))]} == $1 && ${gameBoard[$((row=$row+1))]} == $1 ]]
+		then
+			flag=true
+			break
+		fi
+
+		if [[ ${gameBoard[$col]} == $1 && ${gameBoard[$((col=$col+3))]} == $1 && ${gameBoard[$((col=$col+3))]} == $1 ]]
+		then
+			flag=true
+			break
+		fi
+
+		if [[ ${gameBoard[$diag1]} == $1 && ${gameBoard[$((diag1=$diag1+4))]} == $1 && ${gameBoard[$((diag1=$diag1+4))]} == $1 ]]
+		then
+			flag=true
+			break
+		fi
+
+		if [[ ${gameBoard[$diag2]} == $1 && ${gameBoard[$((diag2=$diag2+2))]} == $1 && ${gameBoard[$((diag2=$diag2+2))]} == $1 ]]
+		then
+			flag=true
+			break
+		fi
+		row=$(( ($i + 1) * 3 ))
+		col=$(( $i + 1 ))
+	done
+		echo $flag
 }
 
 function main(){
 
-	playerMove $( toss )
-	printBoard
+	count=0
+	status=flag
+
+	read play1 play2 < <( toss )
+
+	echo "Player1 $play1"
+	echo "Player2 $play2"
+
+	for (( j=0;j<${#gameBoard[@]};j++ ))
+	do
+		if [[ $play1 == $changeTurn ]]
+		then
+			playerMove $play1
+			printBoard
+			status=$( checkWhoWon $play1 )
+			if [[ $status == true ]]
+			then
+					echo "Player 1 is Won"
+					break
+			fi
+		else
+			playerMove $play2
+			printBoard
+			status=$( checkWhoWon $play2 )
+			if [[ $status == true ]]
+			then
+					echo "Player 2 is Won"
+					break
+			fi
+		fi
+	done
+
+	if [[ $status == false ]]
+	then
+		echo "Game Tie.."
+	fi
 }
 
 main
