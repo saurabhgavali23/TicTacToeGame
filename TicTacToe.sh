aaee2adfeac992314b3e83a5498ec42352ed9603
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-17 10:10:33 +0530

    ADDED: Print Board Fun. And Player Position Fun.

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -13,12 +13,42 @@ function toss(){
 	case $toss in
 
 		0)
-			player=o
-			computer=x;;
+			echo o;;
 		1)
-			player=x
-			computer=o;;
+			echo x;;
 	esac
-
-	echo $player $computer
 }
+
+function printBoard(){
+
+	row=0
+	count=1
+	for (( i=0;i<3;i++ ))
+	do
+		echo " $((count++))| ${gameBoard[$row]} | $((count++))| ${gameBoard[$((row=$row+1))]} | $((count++))| ${gameBoard[$((row=$row+1))]} |"
+		row=$(($row+1))
+	done
+}
+
+function playerMove(){
+
+	flag=true
+	while [[ $flag == true ]]
+	do
+		read -p "Enter the Position " pos
+
+		if (( $pos <= 9 ))
+		then
+			gameBoard[$(($pos - 1))]=$1
+			flag=false
+		fi
+	done
+}
+
+function main(){
+
+	playerMove $( toss )
+	printBoard
+}
+
+main
